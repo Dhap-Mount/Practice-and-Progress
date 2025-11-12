774148d6c03113058a593c954f3f4be6be994d3e
Dhap_Mount <cb4287802@gmail.com>
2025-11-12 08:55:07 +0800

    add a new method

--- 1.6.7_有趣的跳跃/输入方式.cpp
+++ 1.6.7_有趣的跳跃/输入方式.cpp
@@ -21,4 +21,18 @@ int main()
 	
 	return 0;
 }
+
 // Gemini使用一个 while 语句贯穿了整个 main 函数，这是可行的
+// 在和deepseek的交流中，我发现Gemini的书写方法总是很繁琐，实际上没有必要这样
+
+int input() // 这样写就行，我会去问问Gemini那样写的缘由
+{
+	int n;
+	cin >> n; // 输入第一个值n
+
+	long long arr[n];
+	for (int i = 0; i < n; i ++)
+	{
+		cin >> arr[i];
+	}
+}
